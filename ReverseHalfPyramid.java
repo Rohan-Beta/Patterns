eecf5a8ce80bbb73207f59b7348cf2f417e7cb2e
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-02-23 15:06:42 +0530

    Create ReverseHalfPyramid.java

---
+++ ReverseHalfPyramid.java
@@ -0,0 +1,18 @@
+// reverse half pyramid
+
+import java.util.*;
+
+class Firstclass {
+    public static void main(String args[]) {
+
+        for(int i = 1; i <= 4; i += 1) { // rows
+            for(int j = 4-i; j >= 1; j -= 1) { // columns for space
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i ; j += 1) { // columns for *
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
