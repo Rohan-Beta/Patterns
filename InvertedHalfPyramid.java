1677b8151343952a8bb5e5bb6b80ba891271c482
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-02-23 14:45:55 +0530

    Create InvertedHalfPyramid.java

---
+++ InvertedHalfPyramid.java
@@ -0,0 +1,15 @@
+// inverted half pyramid
+
+import java.util.*;
+
+class Firstclass {
+    public static void main(String args[]) {
+
+        for(int i = 4; i >=1; i -= 1) { // rows
+            for(int j = 1; j <= i; j += 1) { // columns
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
