0b07ba6b6f9b9712f7705fc4132d42b1e3efa1b0
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-02-23 15:20:39 +0530

    Update HalfPyramidWithNumbers(2).java

--- HalfPyramidWithNumbers(2).java
+++ HalfPyramidWithNumbers(2).java
@@ -5,8 +5,8 @@ import java.util.*;
 class Firstclass {
     public static void main(String args[]) {
 
-        for(int i = 1; i <= 4; i += 1) {
-            for(int j = i; j >= 1; j -= 1) {
+        for(int i = 1; i <= 4; i += 1) { //rows
+            for(int j = i; j >= 1; j -= 1) { //columns
                 System.out.print(j);
             }
             System.out.print("\n");
