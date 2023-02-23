2368f9a1afd39ca7f1f91956091b02d1a827385a
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-02-23 19:23:22 +0530

    Create pyramid.java

---
+++ pyramid.java
@@ -0,0 +1,18 @@
+// pyramid
+
+import java.util.*;
+
+class Firstclass {
+    public static void main(String args[]) {
+
+        for(int i = 1; i <= 4; i += 1) { // rows
+            for(int j = 4-i; j >= 1; j -= 1) { //columns space
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j += 1) { // columns for number
+                System.out.print("*" + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
