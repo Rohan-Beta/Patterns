af7f8a5fc1f55fbbccd7e80eccdfab582b8cdce9
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-02-23 15:31:46 +0530

    Update Floyd's Triangle.java

--- Floyd's Triangle.java
+++ Floyd's Triangle.java
@@ -6,8 +6,8 @@ class Firstclass {
     public static void main(String args[]) {
         int num = 1;
 
-        for(int i = 1; i <= 4; i += 1) {
-            for(int j = 1; j <= i; j += 1) {
+        for(int i = 1; i <= 4; i += 1) { // rows
+            for(int j = 1; j <= i; j += 1) { // columns
                 System.out.print(num);
 
                 num += 1;
