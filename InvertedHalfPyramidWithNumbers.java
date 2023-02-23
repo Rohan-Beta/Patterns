370fded08dbcdb928ec824bb95dbaf8156fcfc17
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-02-23 15:21:37 +0530

    Update InvertedHalfPyramidWithNumbers.java

--- InvertedHalfPyramidWithNumbers.java
+++ InvertedHalfPyramidWithNumbers.java
@@ -5,8 +5,8 @@ import java.util.*;
 class Firstclass {
     public static void main(String args[]) {
 
-        for(int i = 4; i >= 1; i -= 1) {
-            for(int j = 1; j <= i; j += 1) {
+        for(int i = 4; i >= 1; i -= 1) { // rows
+            for(int j = 1; j <= i; j += 1) { // columns
                 System.out.print(j);
             }
             System.out.print("\n");
