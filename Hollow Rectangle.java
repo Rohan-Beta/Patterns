498ce457c635e15a3b57a1bb90f8d940f82d63f5
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-02-23 14:35:12 +0530

    Create Hollow Rectangle.java

---
+++ Hollow Rectangle.java
@@ -0,0 +1,21 @@
+// hollow rectangle
+
+import java.util.*;
+
+class Firstclass {
+    public static void main(String args[]) {
+
+        for(int i = 1; i <= 4; i += 1) { //rows
+            for(int j = 1; j <= 5; j += 1) { //columns
+
+                if(i == 1 || j == 1 || i == 4 || j == 5) { //track index
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+}
