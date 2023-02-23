fabc4a206345aedb0c17f18b7fdd0791bc897ef5
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-02-23 19:10:17 +0530

    Create Solid Rhombus.java

---
+++ Solid Rhombus.java
@@ -0,0 +1,18 @@
+// solid rhombus
+
+import java.util.*;
+
+class Firstclass {
+    public static void main(String args[]) {
+
+        for(int i = 1; i <= 4; i += 1) { // rows
+            for(int j = 4-i; j >= 1; j -= 1) { // space
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= 5; j += 1) { // columns
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
