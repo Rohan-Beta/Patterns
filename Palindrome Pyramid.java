c5a270b199e25054bd49cefea6e41c60bf8d7905
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-02-23 19:45:08 +0530

    Create Palindrome Pyramid.java

---
+++ Palindrome Pyramid.java
@@ -0,0 +1,26 @@
+// pallindrome pattern
+
+import java.util.*;
+
+class Firstclass {
+    public static void main(String args[]) {
+
+        for(int i = 1; i <= 4; i += 1) { // rows
+
+            for(int j = 4-i; j >= 1; j -= 1) { //columns space
+                System.out.print(" ");
+            }
+            // first side
+
+            for(int j = i; j >= 1; j -= 1) { // columns for first side
+                System.out.print(j);
+            }
+            // second side
+
+            for(int j = 2; j <= i; j += 1) { // columns for second side
+                System.out.print(j);
+            }
+            System.out.print("\n");
+        }
+    }
+}
