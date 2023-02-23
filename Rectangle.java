955093047c85f6429098f77cc5590a855a4db362
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-02-23 12:27:50 +0530

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Firstclass {
+    public static void main(String args[]) {
+        for(int i = 0; i < 4; i += 1) { // i used for rows
+            for(int j = 0; j < 5; j += 1) {// j used for columns
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
