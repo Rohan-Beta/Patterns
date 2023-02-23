a4779236a4afed664e4c8991bfa9972940b1361f
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-02-23 15:40:25 +0530

    Create 0-1 Triangle.java

---
+++ 0-1 Triangle.java
@@ -0,0 +1,21 @@
+// 0-1 triangle
+
+import java.util.*;
+
+class Firstclass {
+    public static void main(String args[]) {
+
+        for(int i = 1; i <= 5; i += 1) {
+            for(int j = 1; j <= i; j += 1) {
+
+                if((i+j)%2 == 0) {
+                    System.out.print(1);
+                }
+                else {
+                    System.out.print(0);
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+}
