acdb05864004d02c6fe917e0b7a916617b626890
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-02-23 14:39:35 +0530

    Create Half Pyramid.java

---
+++ Half Pyramid.java
@@ -0,0 +1,15 @@
+// half pyramid
+
+import java.util.*;
+
+class Firstclass {
+    public static void main(String args[]) {
+     
+        for(int i = 1; i <= 4; i += 1) { // rows
+            for(int j = 1; j <= i; j += 1) { // columns
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
