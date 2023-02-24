040eeb2dc080fc5bc070cec0faf905f38a22a1a9
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-02-24 21:45:04 +0530

    Update 0-1 Triangle.java

--- 0-1 Triangle.java
+++ 0-1 Triangle.java
@@ -9,10 +9,10 @@ class Firstclass {
             for(int j = 1; j <= i; j += 1) { // columns
 
                 if((i+j)%2 == 0) {
-                    System.out.print(1);
+                    System.out.print(1 + " ");
                 }
                 else {
-                    System.out.print(0);
+                    System.out.print(0 + " ");
                 }
             }
             System.out.print("\n");
