18f2f736ca35b382a975195fd10d17359e337e5d
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-09-28 16:49:40 +0530

    Update HalfPyramidWithNumbers.java

--- HalfPyramidWithNumbers.java
+++ HalfPyramidWithNumbers.java
@@ -1,5 +1,14 @@
 // half pyramid with numbers
 
+/*
+
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+
+*/
+
 import java.util.*;
 
 class Firstclass {
@@ -7,7 +16,7 @@ class Firstclass {
 
         for(int i = 1; i <= 4; i += 1) { // rows
             for(int j = 1; j <= i; j += 1) { // columns
-                System.out.print(j);
+                System.out.print(j + " ");
             }
             System.out.print("\n");
         }
