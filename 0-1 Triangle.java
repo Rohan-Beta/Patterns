c395f278126070078b64fe2eb8565be74a945e54
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-09-28 16:42:56 +0530

    Update 0-1 Triangle.java

--- 0-1 Triangle.java
+++ 0-1 Triangle.java
@@ -1,14 +1,26 @@
 // 0-1 triangle
 
+/*
+    1 
+   0 1 
+  1 0 1 
+ 0 1 0 1 
+1 0 1 0 1
+
+*/
+
 import java.util.*;
 
 class Firstclass {
     public static void main(String args[]) {
 
-        for(int i = 1; i <= 5; i += 1) { // rows
-            for(int j = 1; j <= i; j += 1) { // columns
+        for(int i = 1; i <= 5; i += 1) {
+            for(int j = 5-i; j >= 1; j -= 1) {
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j += 1) {
 
-                if((i+j)%2 == 0) {
+                if((i+j) % 2 == 0) {
                     System.out.print(1 + " ");
                 }
                 else {
